b080da6070b325e952827b65afdbbf4c108f7bf3
sheraz-shaik <shaik786sheraz@gmail.com>
2025-06-28 23:31:26 +0530

    Add files via upload

---
+++ Week-2_JUnit_Basic_Testing/Demo/src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,47 @@
+package com.example;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+        System.out.println("Setup complete");
+    }
+
+    @After
+    public void tearDown() {
+        calculator = null;
+        System.out.println("Cleanup complete");
+    }
+
+    @Test
+    public void testAddition() {
+        // Arrange
+        int a = 10, b = 5;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(15, result);
+    }
+
+    @Test
+    public void testSubtraction() {
+        // Arrange
+        int a = 10, b = 4;
+
+        // Act
+        int result = calculator.subtract(a, b);
+
+        // Assert
+        assertEquals(6, result);
+    }
+}
+
